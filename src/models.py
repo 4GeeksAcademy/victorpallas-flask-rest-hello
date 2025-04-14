8bfa7d2e17f9bc2485ddef4301d3a18b5140580c
victorpallas1 <victorpallas@seguridadea1.com>
2025-04-14 19:20:57 +0000

    primera parte

--- src/models.py
+++ src/models.py
@@ -1,19 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
+    lastname: Mapped[str] = mapped_column(String(60), unique=False, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Follower(db.Model):
+    __tablename__ = "follower"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
